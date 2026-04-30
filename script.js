00d9450a2057a44c9ca74b24cb570e5377baa9d5
laura flor <lauraflorrodrigues@gmail.com>
2026-04-30 16:48:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,83 +1,63 @@
-let ativo1 = document.getElementById("ativo1")
-let texto = document.getElementById("texto")
-let remover = document.getElementById("remover")
-let alternar = document.getElementById("alternar")
-let destaque = document.getElementById("destaque")
-let grande = document.getElementById("grande")
-let escondido = document.getElementById("escondido")
-let hover = document.getElementById("hover")
-let itens = document.getElementsByClassName("item");
+let divs = document.querySelectorAll(".caixa")
+let mudar = document.getElementById("mudar")
 
-ativo1.addEventListener("click", function() {
-texto.classList.add("ativo")
+let testes = document.querySelectorAll(".teste")
+let adicionar = document.getElementById("adicionar")
+
+let opcoes = document.querySelectorAll(".opcao")
+
+let menus = document.querySelectorAll(".menu")
+
+
+
+mudar.addEventListener("click", function(){
+    divs.forEach(function(div){
+        div.classList.toggle("cor")
+    })
 })
 
-remover.addEventListener("click", function() {
-texto.classList.remove("ativo")
+
+
+adicionar.addEventListener("click", function(){
+    testes.forEach(function(t){
+        t.classList.add("ativo")
+    })
 })
 
-alternar.addEventListener("click", function() {
-texto.classList.toggle("ativo")
+
+
+divs.forEach(function(div){
+    div.addEventListener("click", function(){
+        console.log("clicou")
+        div.classList.toggle("rosa")
+        div.classList.toggle("destaque")
+    })
 })
 
-destaque.addEventListener("click", function() {
-texto.classList.toggle("destaque")
+
+
+opcoes.forEach(function(opcao){
+    opcao.addEventListener("click", function(){
+
+        opcoes.forEach(function(o){
+            o.classList.remove("ativo")
+        })
+
+        opcao.classList.add("ativo")
+
+    })
 })
 
-grande.addEventListener("click", function() {
-texto.classList.toggle("grande")
-})
 
-escondido.addEventListener("click", function() {
-texto.classList.toggle("escondido")
-})
 
-hover.addEventListener("mouseover", function() {
-  hover.classList.add("hover")
-});
+menus.forEach(function(menu){
+    menu.addEventListener("click", function(){
 
-hover.addEventListener("mouseout", function() {
-  hover.classList.remove("hover")
-});
+        menus.forEach(function(m){
+            m.classList.remove("ativo")
+        })
 
-botao.addEventListener("click", function() {
-  botao.classList.toggle("mudarBotao")
-});
+        menu.classList.add("ativo")
 
-for (let i = 0; i < itens.length; i++) {
-  itens[i].onclick = function() {
-    itens[i].classList.toggle("selecionado")
-  };
-}
-
-dark.addEventListener("click", function() {
-  document.body.classList.toggle("dark")
-}
-)
-
-menu.addEventListener("click", function() {
-  menu.classList.toggle("cores")
-})
-
-menu.addEventListener("mouseover", function() {
-  menu.classList.add("borda")
-})
-
-menu.addEventListener("mouseout", function() {
-  menu.classList.remove("borda")
-})
-
-red.addEventListener("click", function() {
-  caixa.classList.remove("azul", "verde")
-  caixa.classList.add("vermelho")
-})
-
-blue.addEventListener("click", function() {
-  caixa.classList.remove("vermelho", "verde")
-  caixa.classList.add("azul")
-})
-
-green.addEventListener("click", function() {
-  caixa.classList.remove("vermelho", "azul")
-  caixa.classList.add("verde")
+    })
 })
